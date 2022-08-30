e0b5d6f9dee676c0f1570a39c55f4e22b2b9b15c
cl <1010056799@qq.com>
2022-08-30 15:46:49 +0800

    dev-A分支提交的代码

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
         System.out.println("222222222222222");
         System.out.println("3333333333333");
         System.out.println("44444444444444444444");
+        System.out.println("dev-A分支写的代码：555555555555555");
     }
 }
