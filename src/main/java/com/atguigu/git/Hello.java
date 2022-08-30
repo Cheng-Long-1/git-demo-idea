298f92c787f727dccb821944ddd5711709fa3c4b
cl <1010056799@qq.com>
2022-08-30 16:40:29 +0800

    提交记录

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -8,5 +8,7 @@ public class Hello {
         System.out.println("44444444444444444444");
         System.out.println("dev-A分支写的代码：555555555555555");
         System.out.println("master代码：5555555555555555555555555");
+
+        System.out.println("这个代码新增，推到github上");
     }
 }
